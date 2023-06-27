40d8363fc1f96e0feac86526bf908cbcd6d6367a
TDSOFT <dariusz@tdsoft.pl>
2023-06-27 23:02:06 +0200

    Initial Commit

---
+++ DailyGamification/DailyGamificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyGamificationApp.swift
+//  DailyGamification
+//
+//  Created by Dariusz Tarczynski on 27/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct DailyGamificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
